58b32cde96b662793976e1c47ae6f3f826955e0e
Sweekarplayz <70138621+Sweekarpp@users.noreply.github.com>
2021-02-04 21:43:11 +0530

    deded

--- sketch.js
+++ sketch.js
@@ -4,33 +4,37 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var player,ground,mountains,playerimage,zombie,sword,swordImg;
+var player,ground,mountains,playerimage,zombie,sword,swordImg,creeperImg,invisGround;
 var canvas;
-var rand = Math.round(random(1,2));
+var itemsGroup;
+//var rand = Math.round(random(1,2));
 
 function preload() {
   playerimage = loadImage("my player.png");
   mountains = loadImage("my background.jpg");
   swordImg = loadImage("netherite sword.png");
+  creeperImg = loadImage('charged creeper.png');
 }
 
 function setup(){
 canvas = createCanvas(displayWidth,displayHeight-125);
 engine = Engine.create();
 world = engine.world;
+invisGround = createSprite(900,600,2000,10);
 ground = new Ground(900,620,2000,10);
   player = Bodies.rectangle(100,425,450,400,{
     isStatic: true
   });
   World.add(world,player);
-  if(rand === 1){
-    zombie = new zombies(300,300,100,200);
-  }else if(rand === 2){
+  //if(rand === 1){
+  //zombie = new zombies(300,300,100,200);
+  //}else if(rand === 2){
   zombie = new zombies(300,300,200,400);
-}
+//}
   sword = createSprite(player.position.x+120,player.position.y-20,250,200);
   sword.addImage("sword",swordImg);
   sword.scale = 8;
+  itemsGroup = new Group();
 }
 
 function draw(){
@@ -48,21 +52,34 @@ function draw(){
       ground.body.position.x = 900;
     }
     creepers();
-    if(rand === 1 || rand === 2){
+   
+    //if(rand === 1 || rand === 2){
     zombie.display();
-  }
+  //}
+    
     drawSprites();
+    spawnpoint();
 }
 
 function creepers(){
-  creeper = Bodies.rectangle(800,100,100,100,{
+  creeper = Bodies.rectangle(800,200,100,100,{
     isStatic:false
   });
   World.add(world,creeper);
   push();
   translate(creeper.position.x,creeper.position.y);
   rotate(creeper.angle);
-  rectMode(CENTER);
-  rect(0,0,100,100);
+  imageMode(CENTER);
+  image(creeperImg,0,0,250,400);
   pop();
+}
+
+function spawnpoint(){
+  if(frameCount%100 === 0){
+  item = createSprite(random(0,900),20,20,20);
+  item.velocityY = 3;
+  
+  itemsGroup.add(item);
+  itemsGroup.collide(invisGround);
+  }
 }
