0ec3b2267f479c7e7e6ee7b68add7dc920e5b9c4
Sweekarplayz <70138621+Sweekarpp@users.noreply.github.com>
2021-02-08 17:20:03 +0530

    LOL2

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var player,ground,mountains,playerimage,zombie,sword,swordImg,creeperImg,invisGround;
+var player,ground,mountains,playerimage,zombie,sword,swordImg,creeperImg,invisGround,playerSprite;
 var canvas;
-var itemsGroup;
+var itemsGroup,item1,item2,item3,item4,item5,item6;
 //var rand = Math.round(random(1,2));
 
 function preload() {
@@ -14,6 +14,12 @@ function preload() {
   mountains = loadImage("my background.jpg");
   swordImg = loadImage("netherite sword.png");
   creeperImg = loadImage('charged creeper.png');
+  item1 = loadImage("stick.png");
+  item2 = loadImage("ruby.png");
+  item3 = loadImage("iron ingot.png");
+  item4 = loadImage("gold ingot.png");
+  item5 = loadImage("diamond.png");
+  item6 = loadImage("emerald.png");
 }
 
 function setup(){
@@ -21,7 +27,10 @@ canvas = createCanvas(displayWidth,displayHeight-125);
 engine = Engine.create();
 world = engine.world;
 invisGround = createSprite(900,600,2000,10);
+invisGround.visible = false;
 ground = new Ground(900,620,2000,10);
+playerSprite = createSprite(100,425,450,400);
+playerSprite.visible = false;
   player = Bodies.rectangle(100,425,450,400,{
     isStatic: true
   });
@@ -34,6 +43,7 @@ ground = new Ground(900,620,2000,10);
   sword = createSprite(player.position.x+120,player.position.y-20,250,200);
   sword.addImage("sword",swordImg);
   sword.scale = 8;
+  sword.visible = false;
   itemsGroup = new Group();
 }
 
@@ -58,7 +68,7 @@ function draw(){
   //}
     
     drawSprites();
-    spawnpoint();
+    items();
 }
 
 function creepers(){
@@ -74,12 +84,28 @@ function creepers(){
   pop();
 }
 
-function spawnpoint(){
+function items(){
   if(frameCount%100 === 0){
   item = createSprite(random(0,900),20,20,20);
   item.velocityY = 3;
-  
+  switch(Math.round(random(1,6))){
+    case 1:item.addImage(item1);
+    break;
+    case 2:item.addImage(item2);
+    break;
+    case 3:item.addImage(item3);
+    break;
+    case 4:item.addImage(item4);
+    break;
+    case 5:item.addImage(item5);
+    break;
+    case 6:item.addImage(item6);
+    break;
+  }
   itemsGroup.add(item);
   itemsGroup.collide(invisGround);
+  if(itemsGroup.isTouching(playerSprite)){
+  itemsGroup.destroyEach();
+  }
   }
 }
